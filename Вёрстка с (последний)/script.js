13241d7d8040172a617a51cbac180bd992923170
Evgenij313 <166474338+Evgenij313@users.noreply.github.com>
2024-06-04 16:22:42 +0000

    deleted folder

--- Вёрстка с (последний)/script.js
+++
@@ -1,6 +0,0 @@
-const menu = document.querySelector(".mobile_menu")
-const button = document.querySelector(".burger-menu")
-
-button.addEventListener("click",()=>{
-    menu.classList.toggle("visible");
-})
